d3ed4c6efef7ee3e48ec9fe43ce86a180b662835
Dorothee Hueser <dorothee.hueser-espig@ptb.de>
2019-10-10 14:10:25 +0200

    reducing number of lines of code

--- filters/filtergaussianregression.py
+++ filters/filtergaussianregression.py
@@ -13,22 +13,19 @@ def gaussian_reg_p1(dx, heights, lamb):
     nlambda = int(np.floor(lamb/dx)) + 1
     x_1 = np.array(np.arange(0, numh)*dx)
     x_2 = np.concatenate((x_1, np.array(np.arange(-nlambda, 0, 1)*dx)))/alpha_lamb
-    heightextended = np.zeros(len(x_2))
-    heightextended[0:numh] = heights
 #
-    sgauss = np.exp(-np.pi*np.square(x_2))/(alpha_lamb)
 # (1) Fourier trafos fuer die Inhomogenitaet des Gl-sys.:
 # (1.a) z-Werte:
 #     dazu fuer die periodische Fortsetzung verlaengern
+    heightextended = np.zeros(len(x_2))
+    heightextended[0:numh] = heights
     fourier_height = np.fft.fft(heightextended)
 # (1.b)
+    sgauss = np.exp(-np.pi*np.square(x_2))/(alpha_lamb)
     fourier_sgauss = np.fft.fft(sgauss)
-    sgaussx = np.multiply(x_2, sgauss)
-    fourier_sgaussx = np.fft.fft(sgaussx)
-    fourier_r1 = np.multiply(fourier_height, fourier_sgauss)
-    fourier_r2 = np.multiply(fourier_height, fourier_sgaussx)
-    r_1 = np.fft.ifft(fourier_r1) * dx
-    r_2 = np.fft.ifft(fourier_r2) * dx
+    fourier_sgaussx = np.fft.fft(x_2*sgauss)
+    r_1 = np.fft.ifft(fourier_height*fourier_sgauss) * dx
+    r_2 = np.fft.ifft(fourier_height*fourier_sgaussx) * dx
 #
 # (2) Gleichungssystem des RG-Filters loesen
 # (1.a) Momentenmatrix, hier die Gaussglocke
@@ -37,8 +34,7 @@ def gaussian_reg_p1(dx, heights, lamb):
     x_3 = x_1
     x_3[numh-nlambda:numh] = np.flip(x_1[0:nlambda], axis=0)
     sgauss3 = np.exp(-np.pi*np.square(x_3/alpha_lamb))
-    mu0 = special.erf(np.sqrt(np.pi)*x_3/(alpha_lamb))
-    mu0 = 0.5*(mu0 + 1)
+    mu0 = 0.5*(special.erf(np.sqrt(np.pi)*x_3/(alpha_lamb)) + 1)
     mu1 = -(alpha_lamb / (2.0*np.pi)) * sgauss3
     mu2 = (alpha_lamb**2 / (2.0*np.pi)) * (mu0 - (x_3/alpha_lamb) * sgauss3) 
 #
